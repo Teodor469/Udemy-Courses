14d422f359732932ed8b7196052fbf4a5510a363
Teodor469 <lifetroll6@gmail.com>
2024-02-21 12:05:13 +0200

    New commit

--- index.php
+++ index.php
@@ -1,11 +1,23 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <p>Hello world</p>
-</body>
-</html>
+<?php
+    $name = "Bro Code";
+    $food = "Pizza";
+    $email = "fake@gmail.com";
+
+    $age = 21;
+    $users = 2;
+    $quantity = 3;
+
+    $gpa = 2.5;
+    $price = 4.99;
+    $tax_rate = 5.1;
+
+    $employed = true;
+    $online = false;
+    $for_sale = true;
+
+    $total = null;
+
+    echo "You have orderd {$quantity} x {$food}<br>";
+    $total = $quantity * $price;
+    echo "The price is {$total}";
+?>
