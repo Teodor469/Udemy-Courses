cdfca38d110abd2cab30f41c0c3c526d3968a5e0
Teodor469 <lifetroll6@gmail.com>
2024-02-22 13:55:28 +0200

    .

---
+++ arrays.php
@@ -0,0 +1,15 @@
+<?php
+
+    $foods = array("apple", "orange", "banana", "coconut");
+
+    // array_push($foods, "pineapple", "kiwi");
+    // array_pop($foods);
+    // array_shift($foods);
+    // $reversed_foods = array_reverse($foods);
+    // echo count($foods);
+
+    foreach($foods as $food){
+        echo $food . "<br>";
+    }
+
+?>
