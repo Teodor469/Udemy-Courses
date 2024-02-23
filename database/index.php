dcd89c587d157916f5d4ee7a0030f3684d59f300
Teodor469 <lifetroll6@gmail.com>
2024-02-23 15:09:40 +0200

    .

--- database/index.php
+++ database/index.php
@@ -2,22 +2,38 @@
 
     include("database.php");
 
-    $username = "Patrick";
-    $password = "rock3";
-    $hash = password_hash($password, PASSWORD_DEFAULT);
+    // $username = "Patrick";
+    // $password = "rock3";
+    // $hash = password_hash($password, PASSWORD_DEFAULT);
 
-    $sql = "INSERT INTO users (user, password) VALUES ('$username', '$hash')";
+    // $sql = "INSERT INTO users (user, password) VALUES ('$username', '$hash')";
 
     
 
-    try{
-        mysqli_query($conn, $sql);
-        echo "User is now registered";
+    // try{
+    //     mysqli_query($conn, $sql);
+    //     echo "User is now registered";
+    // }
+    // catch(mysqli_sql_exception){
+    //     echo "Could not register user";
+    // }
+
+    $sql = "SELECT * FROM users";
+    $result = mysqli_query($conn, $sql);
+
+    if(mysqli_num_rows($result) > 0){
+        while($row = mysqli_fetch_assoc($result)){
+            echo $row["id"] . "<br>";
+            echo $row["user"] . "<br>";
+            echo $row["reg_date"] . "<br>";
+        };
+        
     }
-    catch(mysqli_sql_exception){
-        echo "Could not register user";
+    else{
+        echo "No user found";
     }
 
+    mysqli_close($conn);
 
 ?>
 
