1f1c829c146ca3f595f6d24291c50ebe539e6ebe
Teodor469 <lifetroll6@gmail.com>
2024-02-23 15:33:09 +0200

    .

--- database/index.php
+++ database/index.php
@@ -38,6 +38,9 @@
 ?>
 
 
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -46,6 +49,42 @@
     <title>Document</title>
 </head>
 <body>
-    Hello
+    <form action="<?php htmlspecialchars($_SERVER["PHP_SELF"]) ?>" method="post">
+        <h2>Welcom to Fakebook!</h2>
+        username: <br>
+        <input type="text" name="username"><br>
+        password: <br>
+        <input type="password" name="password"><br>
+        <input type="submit" name="submit" value="register">
+    </form>
 </body>
 </html>
+
+<?php
+
+    if($_SERVER["REQUEST_METHOD"] == "POST"){
+        $username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_SPECIAL_CHARS);
+        $password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_SPECIAL_CHARS);
+
+        if(empty($username)){
+            echo "Please enter a username";
+        }
+        elseif(empty($password)){
+            echo "Please enter a password";
+        }
+        else{
+            $hash = password_hash($password, PASSWORD_DEFAULT);
+            $sql = "INSERT INTO users (user, password) VALUES ('$username', '$hash')";
+
+            try{
+                mysqli_query($conn, $sql);
+                echo "You are now registered";
+            }
+            catch(mysqli_sql_exception){
+                echo "That username is taken";
+            }
+            
+        }
+    }
+    mysqli_close($conn);
+?>
