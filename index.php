1cc39c97166362afc5e7b733eb4ba8d66ebca32f
Teodor469 <lifetroll6@gmail.com>
2024-02-27 13:33:46 +0200

    .

--- index.php
+++ index.php
@@ -1,34 +1,20 @@
-<?php
-    $first_name = 'John';
-    $last_name = "Doe";
-
-    $full_name = $first_name . ' ' . $last_name
-?>
-
-
-
 <!DOCTYPE html>
 <html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://cdn.tailwindcss.com"></script>
-    <title>Learn PHP From Scratch</title>
-</head>
-
-<body class="bg-gray-100">
-    <header class="bg-blue-500 text-white p-4">
-        <div class="container mx-auto">
-            <h1 class="text-3xl font-semibold">Learn PHP From Scratch</h1>
-        </div>
-    </header>
-    <div class="container mx-auto p-4 mt-4">
-        <div class="bg-white rounded-lg shadow-md p-6">
-            <?= "Hello my name is " . $full_name . '<br>'; ?>
-            <?= "Hello my name is {$full_name}<br>"  ?>
-        </div>
-    </div>
-</body>
-
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Brad's PHP Blog | Introduction to PHP</title>
+  </head>
+  <body>
+    <main>
+      <h1>Introduction to PHP</h1>
+      <p>By: John Doe</p>
+      <p>
+        PHP (Hypertext Preprocessor) is a widely used server-side scripting
+        language that has revolutionized web development. With its simplicity,
+        flexibility, and vast community support, PHP has become the backbone of
+        countless dynamic websites and web applications.
+      </p>
+    </main>
+  </body>
 </html>
---
+++ test.php
@@ -0,0 +1,20 @@
+<?php
+
+$number1 = 5;
+$number2 = 10;
+$number3 = '20';
+$fruit = "apple";
+$bool1 = true;
+$bool2 = false;
+$null = null;
+
+$result = $number1 + $number3;
+$result = $number3 . $number2;
+$result = $fruit . $number2;
+$result = $number1 + $bool1;
+$result = $number1 + $null;
+$result = (string) $number1;
+
+var_dump($result)
+
+?>
