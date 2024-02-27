731bbb3e1fb29ab893d9a553b7236a695c14fbd9
Teodor469 <lifetroll6@gmail.com>
2024-02-27 14:20:39 +0200

    .

--- index.php
+++ index.php
@@ -1,14 +1,23 @@
+<?php
+    $title = "Introduction to PHP";
+    $author = "By: John Doe";
+    $pageTitle = "Brad's PHP Blog";
+
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Brad's PHP Blog | Introduction to PHP</title>
+    <title><?php echo "{$title}" . ' | ' . "{$pageTitle}" ?></title>
   </head>
   <body>
     <main>
-      <h1>Introduction to PHP</h1>
-      <p>By: John Doe</p>
+      <h1><?php echo "{$title}" ?></h1>
+      <p><?php echo "$author" ?></p>
       <p>
         PHP (Hypertext Preprocessor) is a widely used server-side scripting
         language that has revolutionized web development. With its simplicity,
