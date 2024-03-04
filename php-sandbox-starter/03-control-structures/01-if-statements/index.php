a7d26e98afaf5d7c5ca4a77b5173452f2715f7b8
Teodor469 <lifetroll6@gmail.com>
2024-03-04 09:54:51 +0200

    .

--- php-sandbox-starter/03-control-structures/01-if-statements/index.php
+++ php-sandbox-starter/03-control-structures/01-if-statements/index.php
@@ -1,10 +1,20 @@
 <?php
-
+$age = 30;
 // If statement
+if ($age >= 21) {
+    echo 'You are old enough to drink in the US';
+    // Code to be executed if the condition is true
+} else{
+    echo 'You are not old enough.';
+}
 
 // If-Else
 
 // Nested if statement
 
 // If-Else-If
-
+if($age >= 21) {
+    echo 'asjdn';
+} else if($age < 18){
+    echo 'asdkjhd';
+}
