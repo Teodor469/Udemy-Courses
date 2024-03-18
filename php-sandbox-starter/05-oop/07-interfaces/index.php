6154fce2edcdc58e6a31572c16af4c03a79ba86a
Teodor469 <lifetroll6@gmail.com>
2024-03-18 15:09:36 +0200

    .

--- php-sandbox-starter/05-oop/07-interfaces/index.php
+++ php-sandbox-starter/05-oop/07-interfaces/index.php
@@ -1,3 +1,59 @@
+<?php
+  interface ContentInterface {
+    public function display();
+    public function edit();
+
+  }
+
+  class Article implements ContentInterface {
+    private $title;
+    private $content;
+
+    public function __construct($title, $content)
+    {
+      $this->title = $title;
+      $this->content = $content;
+    }
+
+    public function display() {
+      echo "<h2>{$this->title}</h2>";
+      echo "<p>{$this->content}</p>";
+    }
+
+    public function edit() {
+      echo "Editing the article '{$this->title}";
+    }
+  }
+
+
+  class Video implements ContentInterface {
+    private $title;
+    private $url;
+
+    public function __construct($title, $url)
+    {
+      $this->title = $title;
+      $this->url = $url;
+    }
+
+    public function display() {
+      echo "<h2>{$this->title}</h2>";
+      echo "<iframe src='{$this->url}'></iframe>";
+    }
+
+    public function edit() {
+      echo "Editing the video '{$this->title}";
+    }
+  }
+
+
+$article = new Article('Introduction to PHP', 'PPHP is a versitile scripting language');
+
+$video = new Video('PHP for beginners: ', 'https://www.udemy.com/course/php-from-scratch-course/learn/lecture/41057860#content');
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -16,7 +72,10 @@
   </header>
   <div class="container mx-auto p-4 mt-4">
     <div class="bg-white rounded-lg shadow-md p-6 mt-6">
-      <!-- Output -->
+      <?=$article->display() ?>
+    </div>
+    <div class="bg-white rounded-lg shadow-md p-6 mt-6">
+      <?=$video->display() ?>
     </div>
   </div>
 </body>
