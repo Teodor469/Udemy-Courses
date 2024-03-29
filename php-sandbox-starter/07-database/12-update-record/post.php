9ebd028989ed09f33b26ce5e1f30d4b72e957aba
Teodor469 <lifetroll6@gmail.com>
2024-03-29 10:48:31 +0200

    .

--- php-sandbox-starter/07-database/12-update-record/post.php
+++ php-sandbox-starter/07-database/12-update-record/post.php
@@ -11,7 +11,7 @@ if (!$id) {
 }
 
 // SELECT statement with placeholder for id
-$sql = 'SELECT * FROM posts WHERE id = :id';
+$sql = 'SELECT * FROM post WHERE id = :id';
 
 // Prepare the SELECT statement
 $stmt = $pdo->prepare($sql);
@@ -51,10 +51,14 @@ $post = $stmt->fetch();
           <a href="index.php">Go Back</a>
         </div>
       </div>
+      <!-- Edit button -->
+      <a href="edit.php?id=<?= $post['id'] ?>" class="bg-green-500 text-white px-4 py-2 rounded block w-full text-center mb-4 hover:bg-green-600 focus:outline-none">Edit</a>
+
+
       <form action="delete.php" method="post">
         <input type="hidden" name="_method" value="delete">
         <input type="hidden" name="id" value="<?= $post['id'] ?>">
-        <button type="submit" name="submit" class="text-xl bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600 focus:outline-none">Delete</button>
+        <button type="submit" name="submit" class="text-xl bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600 focus:outline-none w-full">Delete</button>
       </form>
     </div>
   </div>
