8f4c02a6cd8dd6a739c1c29ef9472d3455c68ab6
Teodor469 <lifetroll6@gmail.com>
2024-04-07 15:28:26 +0300

    .

--- views/partials/bottom-banner.php
+++ views/partials/bottom-banner.php
@@ -9,7 +9,7 @@
           </p>
         </div>
         <a
-          href="post-job.html"
+          href="/udemy-course-first/public/index.php/listings/create"
           class="bg-yellow-500 hover:bg-yellow-600 text-black px-4 py-2 rounded hover:shadow-md transition duration-300"
         >
           <i class="fa fa-edit"></i> Post a Job
