c4591103c4c83125a9e4136d6d28f0d2e1e0c80b
Teodor469 <lifetroll6@gmail.com>
2024-04-08 20:33:48 +0300

    .

--- public/index.php
+++ public/index.php
@@ -1,18 +1,7 @@
 <?php
 require '../helpers.php';
 
-$routes = [
-    '/udemy-course-first/public/index.php' => 'controllers/home.php',
-    '/udemy-course-first/public/index.php/listings' => 'controllers/listings/index.php',
-    '/udemy-course-first/public/index.php/listings/create' => 'controllers/listings/create.php',
-    '404' => 'controllers/error/404.php'
-];
-
 
 $uri = $_SERVER['REQUEST_URI'];
 
-if(array_key_exists($uri, $routes)) {
-    require(basePath($routes[$uri]));
-} else {
-    require basePath($routes['404']);
-}
+require basePath('router.php');
---
+++ routes.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    '/udemy-course-first/public/index.php' => 'controllers/home.php',
+    '/udemy-course-first/public/index.php/listings' => 'controllers/listings/index.php',
+    '/udemy-course-first/public/index.php/listings/create' => 'controllers/listings/create.php',
+    '404' => 'controllers/error/404.php'
+];
